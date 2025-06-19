1ab51b0a06a41fabe9cd91479f2c563deb716555
dhruvds12 <dhruv.devgan@yahoo.com>
2025-06-19 12:57:31 +0100

    minor

--- feature-ble/src/main/java/com/example/disastermesh/feature/ble/ui/ChatListScreen.kt
+++ feature-ble/src/main/java/com/example/disastermesh/feature/ble/ui/ChatListScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
-//import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
